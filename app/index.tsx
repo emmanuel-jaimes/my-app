bd414330a92381ece9ba7664f9420ade006f6811
emmanuel-jaimes <jaimes.emmanuel@gmail.com>
2024-06-07 17:59:31 -0500

    button behaviors

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Text, View, Button, Pressable, Image, Modal } from "react-native";
-const iconImg = require("/Users/ejaimes/my-app/assets/images/react-logo.png");
+import { Text, View, Button, Pressable, Image, Modal, StatusBar, ActivityIndicator, Alert } from "react-native";
+const iconImg = require("/Users/ejaimes/my-app/assets/images/icon.png");
 
 
 export default function Index() {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <View
       style={{
@@ -13,18 +14,22 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+
+      <StatusBar backgroundColor={'blue'} barStyle={'default'} hidden/>
+      <ActivityIndicator animating={isLoading} size={'large'} color={'blue'}/>
+
       <Text>Hello world </Text>
-      <Text>This is my first app</Text>
+      <Text></Text>
 
       <Button 
       title="Press" 
-      onPress={console.log("Button pressed")}
+      onPress={() => setIsLoading(!isLoading)}
       color="pink"
       >
       </Button>
     
       <Pressable onPress={() => console.log("Image pressed")}>
-        <Image source={iconImg} style={{width: 300, height: 300, backgroundColor: 'black',}} />
+        <Image source={iconImg} style={{width: 150, height: 150, backgroundColor: 'black',}} />
       </Pressable>
 
       <Pressable onPress={() => setIsModalVisible(true) } style={{
@@ -43,14 +48,46 @@ export default function Index() {
       presentationStyle="pageSheet">
         
         <View style={{flex:1, backgroundColor: "pink", padding: 60}}>
-          <Text>Hello Content</Text>
+          <Text>More Scratch Paper</Text>
           <Button 
             title="close"
             color={'midnightBlue'}
-            onPress={() => setIsModalVisible(false)}></Button>
+            onPress={() => setIsModalVisible(false)}>
+          </Button>
+          <Button
+            title="Alert"
+            color={'green'}
+            onPress={() => Alert.alert("you have been alerted!")}>
+          </Button>
+          <Pressable 
+          onPress={() => Alert.alert("Ring the Alarm", "Break in case of emergency",
+            [
+              {
+                text: "Understood",
+
+              },
+              {
+                text: "Break anyway",
+              },
+            ])
+          }
+          style={{
+            backgroundColor:'blue',
+            padding: 10, 
+            margin: 10,
+            borderRadius: 15,
+          }}>
+            <Text 
+            style={
+              {
+                color: 'white',
+                textAlign: "center"
+                }}>
+              Custom Alert Button</Text>
+          </Pressable>
+
 
         </View>
-
       </Modal>
 
     </View>
